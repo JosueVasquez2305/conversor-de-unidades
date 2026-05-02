95a6c8d1cc813a4c64483d3236cb786b14e9d5b0
JosueVasquez2305 <josuealexandervasquezportillo@gmail.com>
2026-05-02 14:22:38 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function convert() {
+    //1.Get the number from the input field
+    const inputAmount = document.getElementById("dataUser").value;
+    //2.Get the selection from the dropdown (km-mll or mll-km)
+    const unitSelection = document.getElementById("convert").value;
+    //3.THE DECISION (step 5 of your plan)
+    if (unitSelection === "km-mll") {
+   // Option A: KM to MILES
+const result = inputAmount * 0.621;
+document.getElementById("resultDisplay").innerText = "resultt: " + result.toFixed (2) + " mi";
+}
+ else {
+  // Option B: MILES o HM (The Inverse)
+  const result = inputAmount / 0.621;
+  document.getElementById("resultDisplay").innerHTML = "Result: " + result.toFixed(2) + " km";
+ }
+}
